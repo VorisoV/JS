4ac641a5d95d49fe84efd1687a85e85420b0b843
VorisoV <90840699+VorisoV@users.noreply.github.com>
2021-09-16 14:45:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+/*/var tomon1 = 7
+var tomon2 = 2
+var tomon3 = 8
+if (tomon1 + tomon2 == tomon3 && (tomon2 + tomon1) > tomon3 ) {
+    console.log("Yasasa bolmaydi")
+}
+else { console.log("Yasasa boladi") }*/
+/*var kirim = 15621
+var chikim = 15621
+if (kirim > chikim) {
+    console.log("foyda kordik")
+}
+else if (kirim < chikim) {
+    console.log("foyda bolmadi")
+}
+else{
+    console.log("Yiyshib ketdi")
+}*/
+/*var a = 70
+if (a > 90) {
+    console.log(5)
+}
+else if (80 < a < 90) {
+    console.log(4)
+}
+else if (70 < a < 80) {
+    console.log(3)
+}
+else if (60 < a < 70) {
+    console.log(2)
+}
+else if (50 < a < 60) {
+    console.log(1)
+}
+else if (40 < a < 50) {
+    console.log(0)
+}
+else ("tugadi")*/
+/*for(var i =100; i >= 0; i--){
+    console.log(i)
+}*/
+/*var letters = ["a", "b", "c", "d", "e", "f", "g"]
+for (var i = letters.length -1; i >= 0; i--) {
+    console.log(letters[i])
+}*/
+/*for(var i = 0; i < 100; i++){
+    if (i % 7 ==00){
+        console.log(i)
+    }
+}*/
+/*for (var i = 0; i < 100; i++) {
+    if (i % 5 == 0){
+        console.log("Bu 5ga bolinadi")
+    }
+    else{console.log(i)}
+}*/
+/*for (i =0; i < 10; i++ ){
+    console.log(`${i} X 3 = ${i * 3}`)
+}*/
+/*Minasa.toUpperCase*/
+/*var pl = ["Python", "JavaScript", "PHP", "Rust"]
+for (i = 0; i < pl.length; i++) {
+    if (pl[i] == "JavaScript") {
+        console.log(pl[i].toUpperCase())
+    }
+    else {
+        console.log(pl[i])
+    }
+}*/
+/*for (i = 0; i <= 100; i++ ) {
+    console.log("EROR".repeat(i))
+}*/
+/*var j=0
+for (i = 10; i > 0; i--) {
+    j++;
+    console.log(" ".repeat(i) + "*".repeat(j))
+}*/
+/*var j = 0
+for (i = 20; i > 0; i--) {
+    console.log(" ".repeat(i) + "*".repeat(j)+"*"+ "*".repeat(j) + " ".repeat(i))
+    j++;
+}*/
+/*var j = 0
+for (i = 20; i > 0; i--) {
+    if(i == 20){
+        console.log(" ".repeat(i) + "/ \\"+ " ".repeat(i))
+    }
+    console.log(" ".repeat(i) +"/"+ "*".repeat(j) + "*" + "*".repeat(j)+"\\" + " ".repeat(i))
+    j++;
+}*/
